20ca12478df3890896db6436e7566703c5abae58
GaoLe <glghkkll@163.com>
2023-05-29 11:23:25 +0800

    fix bug 多次点击时只获取offsetx时span属性会干扰位置

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -2,14 +2,19 @@ const buttons = document.querySelectorAll('.ripple')
 
 buttons.forEach(button => {
     button.addEventListener('click', function (e) {
-        console.log(e);
-        const x = e.offsetX
-        const y = e.offsetY
-        console.log(x, y);
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
         const circle = document.createElement('span')
         circle.classList.add('circle')
-        circle.style.top = y + 'px'
-        circle.style.left = x + 'px'
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
 
         this.appendChild(circle)
 
